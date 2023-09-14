5d5c72c5abee838eaae985e03ad7ef4163fea585
Harsh Vishwakarma <inbox.harshy@gmail.com>
2023-09-14 20:17:16 +0530

    Added MongoDB configuration and created sample REST API with simple POST and GET methods with DB create and get functions

--- src/main/java/com/springtest/demoapp/controller/EmployeeController.java
+++ src/main/java/com/springtest/demoapp/controller/EmployeeController.java
@@ -3,28 +3,37 @@ package com.springtest.demoapp.controller;
 import java.util.List;
 import java.util.Optional;
 
-import javax.swing.text.html.Option;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springtest.demoapp.dao.EmployeRepositoryM;
 import com.springtest.demoapp.dao.EmployeeRepository;
 import com.springtest.demoapp.entities.Employee;
+import com.springtest.demoapp.entities.EmployeeMongo;
 
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @GetMapping("/employee/{id}/")
-    public ResponseEntity<Employee> getEmployee(@PathVariable(value = "id") int id,  HttpServletResponse response){
+    @Autowired
+    private EmployeRepositoryM employeRepositoryMongo;
+
+    @GetMapping("/{id}/")
+    public ResponseEntity<Employee> getEmployee(@PathVariable(value = "id") int id){
         Employee employee = employeeRepository.findById(id);
         if(employee == null){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -33,7 +42,7 @@ public class EmployeeController {
     }
 
     
-    // @GetMapping("/employee/{id}/")
+    // @GetMapping("/{id}/")
     // public Employee getEmployee(@PathVariable(value = "id") int id,  HttpServletResponse response){
     //     Employee employee = employeeRepository.findById(id);
     //     if(employee == null){
@@ -41,4 +50,22 @@ public class EmployeeController {
     //     }
     //     return employee;
     // }
+
+    @PostMapping("/mongo/")
+    public ResponseEntity<EmployeeMongo> getEmployeeFromMongo(@RequestBody EmployeeMongo employeeMongo){
+        EmployeeMongo employee = this.employeRepositoryMongo.save(employeeMongo);
+        if(employee == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(employee));
+    }
+
+    @GetMapping("/mongo/{id}/")
+    public ResponseEntity<EmployeeMongo> getEmployeeFromMongo(@PathVariable(value = "id") int id){
+        Optional<EmployeeMongo> employee = this.employeRepositoryMongo.findById(id);
+        if(employee == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(employee);
+    }
 }
